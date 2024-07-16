7452a07451a2cc0e52b3b930ce3fab4bb342f079
Josh Ratificar <notjoosh@gmail.com>
2024-07-16 16:22:42 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/hiyv/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hiyv
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.hiyv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Preparing glide
+        Glide.with(this).load(R.drawable.backdrop_authentication).into(binding.gifContainer)
+    }
+}
